36af0a9ffa941ad223d72a00aa888285d9830f31
Ryryog25 <60193408+ryryog25@users.noreply.github.com>
2022-07-04 11:16:40 -0500

    Changed Octoprint command
    Edit to be a general "plugin setup" command with both Moonraker and Octoprint options

--- commands/general/OctoPrint.js
+++ commands/general/OctoPrint.js
@@ -4,23 +4,23 @@ const { MessageEmbed } = require('discord.js');
 module.exports = class AppCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'octoprint',
+			name: 'plugin_setup',
 			aliases: ['plugin'],
 			group: 'general',
-			memberName: 'octoprint',
-			description: 'Obico Octoprint plugin',
+			memberName: 'plugin_setup',
+			description: 'Obico plugins for Octoprint and Moonraker',
 		});
 	}
 	run(message) {
 
 		const webAPP = new MessageEmbed()
         .setColor('#5D3FD3')
-        .setTitle('Octoprint')
-        .setURL('https://www.obico.io/docs/user-guides/octoprint-plugin-setup/  ')
+        .setTitle('Plugin Setup')
+        //.setURL('https://www.obico.io/docs/user-guides/octoprint-plugin-setup/  ')
         .setThumbnail('https://yt3.ggpht.com/PPXa-3PVX73Gcd4t0wzR91G8TZ-pt7p9gyJIze2C7MYbaAa54rJqLztq44jPMmZyOTJmJmLJQA=s900-c-k-c0x00ffffff-no-rj')
         .addFields(
             { name: 'Octoprint: ', value: 'https://www.obico.io/docs/user-guides/octoprint-plugin-setup/ ' },
-			{ name: 'moonraker ', value: 'https://github.com/Arksine/moonraker' },
+			{ name: 'Moonraker: ', value: 'https://www.obico.io/docs/user-guides/klipper-setup/ ' },
         )
         .setTimestamp()
         .setFooter('Do -help for more info');
