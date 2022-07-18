ca5a837c421dacc80d7510eac91541327e442f63
Ryryog25 <60193408+ryryog25@users.noreply.github.com>
2022-07-18 13:32:40 -0500

    Update compatibility.js

--- commands/general/compatibility.js
+++ commands/general/compatibility.js
@@ -27,7 +27,7 @@ module.exports = class AppCommand extends Command {
         .setTitle('Compatibility Mode')
         .setThumbnail('https://yt3.ggpht.com/PPXa-3PVX73Gcd4t0wzR91G8TZ-pt7p9gyJIze2C7MYbaAa54rJqLztq44jPMmZyOTJmJmLJQA=s900-c-k-c0x00ffffff-no-rj')
         .addFields(
-            { name: 'How to enable compatibility mode: ', value: 'https://www.obico.io/docs/user-guides/streaming-compatibility-mode/#:~:text=Compatibility%20mode.&text=It%20is%20efficient%20in%20CPU,in%20the%20advanced%20mode%20first.' },
+            { name: 'How to enable compatibility mode: ', value: 'https://www.obico.io/docs/user-guides/streaming-compatibility-mode/#how-to-change-the-compatibility-mode-setting' },
         )
         .setTimestamp()
         .setFooter('Do -help for more info');
