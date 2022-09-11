c2e4eb190420503e2d80cf6f51f5826ecbf7118a
Brandon de Greef <bdegreef5@gmail.com>
2022-09-11 17:54:20 +0200

    FiningREP
    Iam really starting to hate JS and how the variables can just change willy nilly.
    
    IT now works though.

--- commands/reputation/rep.js
+++ commands/reputation/rep.js
@@ -26,36 +26,49 @@ var userAvatarURL = 'nope.nothere';
 
 //const { displayAvatarURL } = await client.users.fetch('159985870458322944')
  // .catch(console.error);
-  
- console.log("User thing is : " + client.users.cache.find(user => user.id == '203924364385583114')); 
- // console.log("\t \t \t USer FULL data is = " + client.guild.cache.user.get('159985870458322944')); 
+try{
+//  console.log("User thing is : " + client.users.cache.find(user => user.id == '203924364385583114')); 
+  // console.log("\t \t \t USer FULL data is = " + client.guild.cache.user.get('159985870458322944')); 
+ 
+}catch(e){console.log(" Error : \n " +e )}
 
 
 
-if(message.mentions.members.first() == null){
+if(message.mentions.users.first() == null){
   userID = message.author.id; 
+  console.log("Message autohro = " + message.author); 
   userAvatarURL = message.author.avatarURL(); 
 
 }else{
-  userID = message.mentions.members.first(); 
-  userAvatarURL =  message.mentions.members.avatarURL(); 
+  console.log("Message mentions = " + message.mentions.users.first() ); 
+  userID = message.mentions.users.first(); 
+  userAvatarURL =  userID.avatarURL(); 
 }
 
 console.log("User ID: " + userID + "\n The user avatar is also : " + userAvatarURL); 
 
+//userAvatarURL = message.author.avatarURL(); 
+//console.log(userAvatarURL);
 
+try{
+  console.log("Data type = " + typeof userID); 
+  userID = userID.id; 
 
-userAvatarURL = message.author.avatarURL(); 
-console.log(userAvatarURL);
+  console.log("Data type = " + userID);
 
-var userScore =  (userData[userID])[0]
-  console.log(userScore);
+  console.log("\t \t \t User data: " + userData["822438562531377162"][0]);
+
+var userScore =  userData[userID][0]; 
+  console.log(" User score = \n \n "  + userScore);
 
   if(userScore == NaN || userScore < 0 || userScore == null){
     console.log("Score not a number!"); 
     userScore = 0;  
   }
-
+}catch(e){
+  console.log(e); 
+  userScore = 0; 
+}
 
   const helpRepLeaderBoard = new Discord.MessageEmbed()
   .setColor('#74EED1')
